e83e34c0d04187fe0be66608d009a6f52c2a4160
AROUNA Drame <arouna.drame@ibm.com>
2019-03-01 15:17:02 +0100

    init

---
+++ src/main/scala/BeatActor.scala
@@ -0,0 +1,47 @@
+package upmc.akka.leader
+
+import akka.actor._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+sealed trait BeatMessage
+case class Beat (id:Int) extends BeatMessage
+case class BeatLeader (id:Int) extends BeatMessage
+
+case class BeatTick () extends Tick
+
+case class LeaderChanged (nodeId:Int)
+
+class BeatActor (val id:Int) extends Actor {
+
+     val time : Int = 50
+     val father = context.parent
+     var leader : Int = 0 // On estime que le premier Leader est 0
+     val schedule = context.system.scheduler.schedule(0 milliseconds, time milliseconds, self, BeatTick)
+
+    def receive = {
+
+         // Initialisation
+        case Start => {
+             self ! BeatTick
+             if (this.id == this.leader) {
+                  father ! Message ("I am the leader")
+             }
+        }
+
+        // Objectif : prevenir tous les autres nodes qu'on est en vie
+        case BeatTick => {
+          if(id == leader) father ! BeatLeader(id)
+          else father ! Beat(id)
+        }
+        case LeaderChanged (nodeId) => {
+          leader = nodeId
+          if (this.id == this.leader) {
+            father ! Message ("I am the new leader after Election")
+          }
+        }
+
+    }
+
+}
---
+++ src/main/scala/CheckerActor.scala
@@ -0,0 +1,48 @@
+package upmc.akka.leader
+
+import java.util
+import java.util.Date
+
+import akka.actor._
+
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+abstract class Tick
+case class CheckerTick () extends Tick
+
+class CheckerActor (val id:Int, val terminaux:List[Terminal], electionActor:ActorRef) extends Actor {
+
+     var time : Int = 200
+     val father = context.parent
+      context.receiveTimeout
+     val schedule = father.scheduler.schedule(0 milliseconds, time milliseconds, self, CheckerTick)
+
+     var nodesAlive:List[Int] = List()
+     var datesForChecking:List[Date] = List()
+     var lastDate:Date = null
+
+     var leader : Int = -1
+
+    def receive = {
+
+         // Initialisation
+        case Start => {
+             self ! CheckerTick
+        }
+
+        // A chaque fois qu'on recoit un Beat : on met a jour la liste des nodes
+        case IsAlive (nodeId) => if(nodesAlive.indexOf(nodeId) == -1) nodesAlive = nodeId::nodesAlive
+        case IsAliveLeader (nodeId) => {
+          leader = nodeId
+          if(nodesAlive.indexOf(nodeId) == -1) nodesAlive = nodeId::nodesAlive
+        }
+
+        // A chaque fois qu'on recoit un CheckerTick : on verifie qui est mort ou pas
+        // Objectif : lancer l'election si le leader est mort
+        case CheckerTick => 
+
+    }
+
+
+}
